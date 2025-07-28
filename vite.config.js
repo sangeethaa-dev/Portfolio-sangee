79ff93fa59d08303349204c7404db722a8cdfaac
Sangeetha A <sangeetha1952003@gmail.com>
2025-07-28 20:28:19 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/", // 👈 this must match your repo name
+  base: '/Portfolio-sangee/', // 👈 this must match your repo name
   plugins: [react()],
 });
 
